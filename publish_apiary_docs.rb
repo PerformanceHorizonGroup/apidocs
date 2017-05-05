15edc73d83437e4aeffac2accf2dab37ebe4d161
Tom Harnasz <tom.harnasz@performancehorizon.com>
2017-05-05 18:49:12 +0100

    Update to publish

--- publish_apiary_docs.rb
+++ publish_apiary_docs.rb
@@ -2,20 +2,25 @@ require 'git'
 
 `mkdir #{Dir.pwd}/.public`
 
-`make`
+`mkdir #{Dir.pwd}/tmp`
+`rsync -avz --exclude 'tmp' . #{Dir.pwd}/tmp/`
 
-g = Git.open(Dir.pwd)
+main_repo = Git.open(Dir.pwd)
+branches = main_repo.branches.remote
 
-branches = g.branches.remote
+cloned_repo = Git.open("#{Dir.pwd}/tmp")
 
 branches.each do |branch|
-	if branch.remote.name == "origin"
-		unless branch.name.include?('->')
-			puts "Publishing apiary docs for branch #{branch.name}"
-			`mkdir -p #{Dir.pwd}/.public/#{branch.name}`
-			`apiary preview --path=\"#{Dir.pwd}/apiary.apib\" --output=\"#{Dir.pwd}/.public/#{branch.name}/index.html\"`
-		end
-	end
+  if branch.remote.name == "origin"
+    unless branch.name.include?('->')
+      `make -C #{Dir.pwd}/tmp`
+      cloned_repo.checkout(branch.name)
+      puts "Publishing apiary docs for branch #{branch.name}"
+      `mkdir -p #{Dir.pwd}/.public/#{branch.name}`
+      `apiary preview --path=\"#{Dir.pwd}/tmp/apiary.apib\" --output=\"#{Dir.pwd}/.public/#{branch.name}/index.html\"`
+    end
+  end
 end
 
+`rm -rf #{Dir.pwd}/tmp`
 `mv #{Dir.pwd}/.public #{Dir.pwd}/public`
