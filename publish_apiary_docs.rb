3b2d5f61d8f4e0830328351778be520005a718e3
Tom Harnasz <tom.harnasz@performancehorizon.com>
2017-05-08 10:02:11 +0100

    Replace HTML title

--- publish_apiary_docs.rb
+++ publish_apiary_docs.rb
@@ -17,10 +17,21 @@ branches.each do |branch|
       `make -C #{Dir.pwd}/tmp`
       puts "Publishing apiary docs for branch #{branch.name}"
       `mkdir -p #{Dir.pwd}/.public/#{branch.name}`
-      `apiary preview --path=\"#{Dir.pwd}/tmp/apiary.apib\" --output=\"#{Dir.pwd}/.public/#{branch.name}/index.html\"`
+
+      output_path = "#{Dir.pwd}/.public/#{branch.name}/index.html"
+      `apiary preview --path=\"#{Dir.pwd}/tmp/apiary.apib\" --output=\"#{document_path}\"`
+      replace_title(branch, output_path)
+
     end
   end
 end
 
 `rm -rf #{Dir.pwd}/tmp`
 `mv #{Dir.pwd}/.public #{Dir.pwd}/public`
+
+
+def replace_title(branch, document_path)
+	text = File.read(document_path) 
+	content = text.gsub(/search_regexp/, "<title> Apiary - #{branch.name} </title>")
+	File.open(filename, "w") { |file| file << content }
+end
