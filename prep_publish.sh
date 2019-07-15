7a5b3189d983160ee57c6dd9709a15a93661711b
Chris Hayen <chris.hayen@partnerize.com>
2019-07-15 15:28:25 -0400

    Updated node

--- prep_publish.sh
+++ prep_publish.sh
@@ -8,11 +8,11 @@ apt-get update -qq && apt-get install -y build-essential
 apt-get -y install locales
 
 # Install node
-curl -sL https://deb.nodesource.com/setup_6.x | bash -;
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 
 # Other tools
 apt-get install -y rsync
 npm install hercule
-npm install apiaryio
+gem install apiaryio
 gem install git
